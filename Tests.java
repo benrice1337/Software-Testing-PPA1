55b74831a32494c2aff2eb509e9a842aadd6ec6a
Ben Rice <dubstepkazoo@Clarence.local>
2019-09-09 13:18:38 -0400

    Test skeleton code.

---
+++ Tests.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.Test;
+
+public class Tests {
+  @Test
+  public void testBodyMassIndex() {
+    //TODO: Tests go here
+  }
+
+  @Test
+  public void testRetirement() {
+    //TODO: Tests go here
+  }
+
+  @Test
+  public void testShortestDistance() {
+    //TODO: Tests go here
+  }
+
+  @Test
+  public void testSplitTip() {
+    //TODO: Tests go here
+  }
+}
