ff6524f47865e860e36966c3bedbbe286d2ffb40
benrice1337 <33522650+benrice1337@users.noreply.github.com>
2019-09-09 17:03:27 +0000

    Add files via upload
    CLI presentational code and function skeletons.

---
+++ PPA1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class PPA1 {
+  public static void main(String[] args) {
+    while(true) {
+      System.out.println("Pick a function:");
+      System.out.println("1. Body Mass Index");
+      System.out.println("2. Retirement");
+      System.out.println("3. Shortest Distance");
+      System.out.println("4. Split the Tip");
+      System.out.println("5. Exit");
+
+      Scanner s = new Scanner(System.in);
+      int choice = s.nextInt();
+
+      if(choice == 1) System.out.println(bodyMassIndex());
+      else if(choice == 2) System.out.println(retirement());
+      else if(choice == 3) System.out.println(shortestDistance());
+      else if(choice == 4) System.out.println(splitTip());
+      else if(choice == 5) break;
+      else System.out.println("Not a valid choice. Try again.");
+    }
+  }
+
+  public String bodyMassIndex() {
+    //TODO: Function body here
+  }
+
+  public String retirement() {
+    //TODO: Function body here
+  }
+
+  public String shortestDistance() {
+    //TODO: Function body here
+  }
+
+  public String splitTip() {
+    //TODO: Funtion body here
+  }
+}
