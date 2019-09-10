b8431c091efc0224894accf6e58bb106bdd2a86f
Ben Rice <dubstepkazoo@Clarence.local>
2019-09-10 10:52:55 -0400

    Retirement done.

--- PPA1.java
+++ PPA1.java
@@ -15,10 +15,24 @@ public class PPA1 {
 
       switch (choice) {
       case 1:
-    	System.out.println(bodyMassIndex(0, 1, 0));
+    	System.out.print("How many feet tall are you (integer only)? ");
+    	int f = s.nextInt();
+    	System.out.print("And how many inches (integer only)? ");
+    	int i = s.nextInt();
+    	System.out.print("How many pounds do you weigh (integer only)? ");
+    	int p = s.nextInt();
+    	System.out.println("Your BMI is " + bmiCalculator(f, i, p) + ", making you" + bodyMassIndex(f, i, p));
     	break;
       case 2:
-    	System.out.println(retirement(0, 0, 0, 0));
+    	System.out.println("How old are you (integer only)? ");
+    	int a = s.nextInt();
+    	System.out.println("What is your salary? ");
+    	int sal = s.nextInt();
+    	System.out.println("What percentage of that do you save (integer only)? ");
+    	int sav = s.nextInt();
+    	System.out.println("What is your goal? ");
+    	int g = s.nextInt();
+    	System.out.println(retirement(a, sal, sav, g));
     	break;
       case 3:
     	System.out.println(shortestDistance());
@@ -57,12 +71,25 @@ public class PPA1 {
   }
 
   public static String retirement(int age, int salary, int saved, int goal) {
-    //TODO: Function body here
-	return "";
+    if (age >= 100) return "Invalid input";
+    if (age <= 0 || salary <= 0 || saved <= 0 || goal <= 0) return "Invalid input";
+    
+    int goalAge = retCalculator(age, salary, saved, goal);
+    if (goalAge >= 100) return "You'll be dead before you reach your goal!";
+	return "You'll reach your goal at age " + goalAge;
   }
   
   public static int retCalculator(int age, int salary, int saved, int goal) {
-	return 0;
+	//Employer matches 35% of what you save
+	double percent = saved * 1.35/ 100.0;
+	double savings = 0;
+	int goalAge = age;
+	
+	while (savings < goal) {
+		savings += salary * percent;
+		++goalAge;
+	}
+	return goalAge;
   }
 
   public static String shortestDistance() {
--- Tests.java
+++ Tests.java
@@ -5,7 +5,6 @@ import org.junit.*;
 public class Tests {
   @Test
   public void testBodyMassIndex() {
-    assertEquals("bodyMassIndex() should return a string", "java.lang.String", PPA1.bodyMassIndex(0, 1, 0).getClass().getName());
     assertEquals("Negative values should not be allowed", "Invalid input", PPA1.bodyMassIndex(5, -8, 140));
     assertEquals("Zero height should not be allowed", "Invalid input", PPA1.bodyMassIndex(0, 0, 140));
     assertEquals("Zero weight should not be allowed", "Invalid input", PPA1.bodyMassIndex(5, 8, 0));
@@ -15,13 +14,18 @@ public class Tests {
 	assertEquals("180 lbs at my height should be overweight", "Overweight", PPA1.bodyMassIndex(5, 8, 180));
 	assertEquals("300 lbs at my height should be obese", "Obese", PPA1.bodyMassIndex(5, 8, 300));
 	
-	//No need to test primitive return types; code wouldn't compile if return type wasn't correct
+	//No need to test return types; code wouldn't compile if return type wasn't correct
 	assertEquals("bmiCalculator() should calculate BMI correctly", 21.3, PPA1.bmiCalculator(5, 8, 140), 0.5);
   }
 
   @Test
   public void testRetirement() {
-    //TODO: Tests go here
+    assertEquals("Age >= 100 should not be allowed", "Invalid input", PPA1.retirement(100, 1, 1, 1));
+	assertEquals("Negative values should not be allowed", "Invalid input", PPA1.retirement(-1, -1, -1, -1));
+	assertEquals("Zero values should not be allowed", "Invalid input", PPA1.retirement(0, 0, 0, 0));
+	
+	assertEquals("Result >= 100 should have special output", "You'll be dead before you reach your goal!", PPA1.retirement(99, 100, 100, 200));
+	assertEquals("Calculation should be correct", "You'll reach your goal at age 24", PPA1.retirement(23, 100, 100, 100));
   }
 
   @Test
