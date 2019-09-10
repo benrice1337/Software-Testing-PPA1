a1dd58ed60e5dfef17cef5ca3cbc17e7a0d89be4
Ben Rice <dubstepkazoo@Clarence.local>
2019-09-09 20:46:02 -0400

    BMI done.

--- PPA1.java
+++ PPA1.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class PPA1 {
   public static void main(String[] args) {
-    while(true) {
+    while (true) {
       System.out.println("Pick a function:");
       System.out.println("1. Body Mass Index");
       System.out.println("2. Retirement");
@@ -13,23 +13,56 @@ public class PPA1 {
       Scanner s = new Scanner(System.in);
       int choice = s.nextInt();
 
-      if(choice == 1) System.out.println(bodyMassIndex());
-      else if(choice == 2) System.out.println(retirement());
-      else if(choice == 3) System.out.println(shortestDistance());
-      else if(choice == 4) System.out.println(splitTip());
-      else if(choice == 5) break;
-      else System.out.println("Not a valid choice. Try again.");
+      switch (choice) {
+      case 1:
+    	System.out.println(bodyMassIndex(0, 1, 0));
+    	break;
+      case 2:
+    	System.out.println(retirement(0, 0, 0, 0));
+    	break;
+      case 3:
+    	System.out.println(shortestDistance());
+    	break;
+      case 4:
+    	System.out.println(splitTip());
+    	break;
+      case 5:
+    	System.out.println("Bye!");
+    	return;
+      default:
+    	System.out.println("Not a valid choice. Try again.");
+    	break;
+      }
     }
   }
 
-  public static String bodyMassIndex() {
+  public static String bodyMassIndex(int feet, int inches, int pounds) {
+    if (feet < 0 || inches < 0 || pounds < 0) return "Invalid input";
+    if (feet == 0 && inches == 0) return "Invalid input";
+    if (pounds == 0) return "Invalid input";
+    
+	double bmi = bmiCalculator(feet, inches, pounds);
+    if (bmi < 18.5) return "Underweight";
+    else if (bmi < 25) return "Normal";
+    else if (bmi < 30) return "Overweight";
+    else return "Obese";
+  }
+  
+  public static double bmiCalculator(int feet, int inches, int pounds) {
+	double kilos = pounds * 0.45;
+	double meters = ((feet * 12) + inches) * 0.025;
+	meters *= meters;
+	double bmi = kilos / meters;
+	return bmi;
+  }
+
+  public static String retirement(int age, int salary, int saved, int goal) {
     //TODO: Function body here
 	return "";
   }
-
-  public static String retirement() {
-    //TODO: Function body here
-	return "";
+  
+  public static int retCalculator(int age, int salary, int saved, int goal) {
+	return 0;
   }
 
   public static String shortestDistance() {
--- Tests.java
+++ Tests.java
@@ -5,8 +5,18 @@ import org.junit.*;
 public class Tests {
   @Test
   public void testBodyMassIndex() {
-    //TODO: Tests go here
-	assertEquals("bodyMassIndex() should return a string", "java.lang.String", PPA1.bodyMassIndex().getClass().getName());
+    assertEquals("bodyMassIndex() should return a string", "java.lang.String", PPA1.bodyMassIndex(0, 1, 0).getClass().getName());
+    assertEquals("Negative values should not be allowed", "Invalid input", PPA1.bodyMassIndex(5, -8, 140));
+    assertEquals("Zero height should not be allowed", "Invalid input", PPA1.bodyMassIndex(0, 0, 140));
+    assertEquals("Zero weight should not be allowed", "Invalid input", PPA1.bodyMassIndex(5, 8, 0));
+    
+	assertEquals("100 lbs at my height should be underweight", "Underweight", PPA1.bodyMassIndex(5, 8, 100));
+	assertEquals("My weight and height should be normal", "Normal", PPA1.bodyMassIndex(5, 8, 140));
+	assertEquals("180 lbs at my height should be overweight", "Overweight", PPA1.bodyMassIndex(5, 8, 180));
+	assertEquals("300 lbs at my height should be obese", "Obese", PPA1.bodyMassIndex(5, 8, 300));
+	
+	//No need to test primitive return types; code wouldn't compile if return type wasn't correct
+	assertEquals("bmiCalculator() should calculate BMI correctly", 21.3, PPA1.bmiCalculator(5, 8, 140), 0.5);
   }
 
   @Test
